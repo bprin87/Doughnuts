070652948b27b79b0b3b7900ebf553449dc1aec8
Ben <123553196+bprin87@users.noreply.github.com>
2024-04-12 14:18:11 +0100

    added alt tags to images

--- src/pages/Doughnuts.jsx
+++ src/pages/Doughnuts.jsx
@@ -25,56 +25,56 @@ function Donuts() {
                             <h3>Original</h3>
                             <p>The one that started it all. Our famous caramel and white chocolate dougnut.</p>
                         </figure>
-                        <img src={selection1}></img>
+                        <img src={selection1} alt="caramel filled doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Fresh Strawberry</h3>
                             <p>The original topped with strawberry jam and shaved almonds.</p>
                         </figure>
-                        <img src={selection2}></img>
+                        <img src={selection2} alt="strawberry filled doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Biscoff Cookie</h3>
                             <p>Not in the mood for a doughnut? Why not try the ultimate Biscoff cookie instead!</p>
                         </figure>
-                        <img src={selection3}></img>
+                        <img src={selection3} alt="biscoff cookie"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Biscoff Doughnut</h3>
                             <p>More doughnut, less cookie. Tastes just as good!</p>
                         </figure>
-                        <img src={selection4}></img>
+                        <img src={selection4} alt="biscoff doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Coffee</h3>
                             <p>If you’re a coffee person then this is the doughnut for you.</p>
                         </figure>
-                        <img src={selection5}></img>
+                        <img src={selection5} alt="coffee flavoured doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Lemon Drizzle</h3>
                             <p>Another original but with a lemon twist.</p>
                         </figure>
-                        <img src={selection6}></img>
+                        <img src={selection6} alt="lemon drizzle doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Chocolate Hazelnut</h3>
                             <p>Nutty chocolate with a caramel centre.</p>
                         </figure>
-                        <img src={selection7}></img>
+                        <img src={selection7} alt="chocolate hazelnut doughnut"></img>
                     </li>
                     <li>
                         <figure>
                             <h3>Apple Crumble</h3>
                             <p>Iced topping with caramel sauce and apples mixed with cinnamon.</p>
                         </figure>
-                        <img src={selection8}></img>
+                        <img src={selection8} alt="apple crumble doughnut"></img>
                     </li>
                 </ul>
             </main>
