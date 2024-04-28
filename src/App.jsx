16e9a87080b3da381107fa96ed83a43351429a94
Ben <123553196+bprin87@users.noreply.github.com>
2024-04-28 16:32:38 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Doughnuts from './pages/Doughnuts'
+import Wholesale from './pages/Wholesale'
+import Contact from './pages/Contact'
 
 function App() {
   
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/doughnuts" element={<Doughnuts/>}/>
+          <Route path="/wholesale" element={<Wholesale/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </Router>
     </>
